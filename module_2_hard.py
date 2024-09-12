bc37f7abba859e000f45a1c5e4db377db6f9fdce
Geddity <92533369+Geddity@users.noreply.github.com>
2024-09-12 22:25:26 +0300

    4

---
+++ module_2_hard.py
@@ -0,0 +1,13 @@
+import random
+
+left_stone = random.randint(3,20)
+right_stone = []
+
+for i in range(1, left_stone):
+    for j in range(i, left_stone):
+        if left_stone % (i+j) == 0 and i != j:
+            right_stone.append(f'{i} + {j}')
+
+result = f'{str(left_stone)} - {', '.join(right_stone)}'
+print(result)
+
