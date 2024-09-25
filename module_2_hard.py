28b1336a29da9824f0445c38a1d2d6b9ba24b227
Geddity <92533369+Geddity@users.noreply.github.com>
2024-09-25 17:22:53 +0300

    5

--- module_2_hard.py
+++ module_2_hard.py
@@ -6,8 +6,8 @@ right_stone = []
 for i in range(1, left_stone):
     for j in range(i, left_stone):
         if left_stone % (i+j) == 0 and i != j:
-            right_stone.append(f'{i} + {j}')
+            right_stone.append(f'{i}{j}')
 
-result = f'{str(left_stone)} - {', '.join(right_stone)}'
+result = f'{str(left_stone)} - {''.join(right_stone)}'
 print(result)
 
